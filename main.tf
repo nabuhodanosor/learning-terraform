2e2d89e995576d638d74af53d4fb3be1d5da32d8
Yevhen <yevgrinman@yahoo.com>
2019-09-17 19:22:45 -0400

    added security_group and asg

--- main.tf
+++ main.tf
@@ -2,11 +2,63 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-07d0cf3af28718ef8"
-  instance_type = "t2.micro"
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-07d0cf3af28718ef8"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-  tags = {
-      Name = "terraform-example"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World!" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 5
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+}
+
+variable "server_port" {
+  description = "The port server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
